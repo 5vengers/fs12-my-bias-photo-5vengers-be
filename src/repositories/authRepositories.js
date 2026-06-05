b86e9025eb0f3ef249ba9df8eb961e840c6c4d5d
apple <apple@apples-MacBook-Air.local>
2026-06-05 13:09:34 +0900

    feat: 회원가입, 로그인, 로그아웃 관련 auth 레포지토리 구현

---
+++ src/repositories/authRepositories.js
@@ -0,0 +1,28 @@
+import prisma from '../config/prisma.js';
+
+const findUserByEmail = (email) => prisma.user.findUnique({ where: { email } });
+
+const findUserByNickname = (nickname) =>
+  prisma.user.findUnique({ where: { nickname } });
+
+const createUser = (data) => prisma.user.create({ data });
+
+const saveRefreshToken = (userId, token, expiresAt) =>
+  prisma.refreshToken.create({
+    data: { userId, token, expiresAt },
+  });
+
+const findRefreshToken = (token) =>
+  prisma.refreshToken.findUnique({ where: { token } });
+
+const deleteRefreshToken = (token) =>
+  prisma.refreshToken.delete({ where: { token } });
+
+export default {
+  findUserByEmail,
+  findUserByNickname,
+  createUser,
+  saveRefreshToken,
+  findRefreshToken,
+  deleteRefreshToken,
+};
