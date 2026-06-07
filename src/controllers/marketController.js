89bbaaa496ba6db3569296a26cf232a961d9560f
sebikawa32 <tpqlsdl150@naver.com>
2026-06-08 01:18:31 +0900

    feat: 마켓 controller 에러처리 추가

--- src/controllers/marketController.js
+++ src/controllers/marketController.js
@@ -1,76 +1,56 @@
-import { marketService } from '../services/marketService';
+import { marketService } from '../services/marketService.js';
 
 export const marketController = {
   //전체 조회
-  getMarketItems: async (req, res, next) => {
-    try {
-      const result = await marketService.getMarketItems();
-      return res.status(200).json({
-        message: '판매 카드 전체 조회 성공',
-        data: result,
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
+  getMarketItems: asyncHandler(async (req, res, next) => {
+    const result = await marketService.getMarketItems();
+    return res.status(200).json({
+      message: '판매 카드 전체 조회 성공',
+      data: result,
+    });
+  }),
 
   //상세 조회
-  getMarketItemDetail: async (req, res, next) => {
-    try {
-      const { itemId } = req.params;
-      const result = await marketService.getMarketItemDetail(Number(itemId));
-      return res.status(200).json({
-        message: '판매 카드 상세 조회 성공',
-        data: result,
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
+  getMarketItemDetail: asyncHandler(async (req, res, next) => {
+    const { itemId } = req.params;
+    const result = await marketService.getMarketItemDetail(Number(itemId));
+    return res.status(200).json({
+      message: '판매 카드 상세 조회 성공',
+      data: result,
+    });
+  }),
 
-  // 3. 판매 등록
-  createMarketItem: async (req, res, next) => {
-    try {
-      const itemData = req.body;
-      const result = await marketService.registerMarketItem(itemData);
-      return res.status(201).json({
-        message: '판매 카드 생성 성공',
-        data: result,
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
+  //  판매 등록
+  createMarketItem: asyncHandler(async (req, res, next) => {
+    const itemData = req.body;
+    const result = await marketService.registerMarketItem(itemData);
+    return res.status(201).json({
+      message: '판매 카드 생성 성공',
+      data: result,
+    });
+  }),
 
-  // 4. 정보 수정
-  updateMarketItem: async (req, res, next) => {
-    try {
-      const { itemId } = req.params;
-      const itemData = req.body;
-      const result = await marketService.updateMarketItem(
-        Number(itemId),
-        itemData,
-      );
-      return res.status(200).json({
-        message: '판매 카드 수정 성공',
-        data: result,
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
+  // 정보 수정
+  updateMarketItem: asyncHandler(async (req, res, next) => {
+    const { itemId } = req.params;
+    const itemData = req.body;
+    const result = await marketService.updateMarketItem(
+      Number(itemId),
+      itemData,
+    );
+    return res.status(200).json({
+      message: '판매 카드 수정 성공',
+      data: result,
+    });
+  }),
 
-  // 5. 판매 삭제
-  deleteMarketItem: async (req, res, next) => {
-    try {
-      const { itemId } = req.params;
-      const result = await marketService.deleteMarketItem(Number(itemId));
-      return res.status(200).json({
-        message: '판매 카드 삭제 성공',
-        data: result,
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
+  // 판매 삭제
+  deleteMarketItem: asyncHandler(async (req, res, next) => {
+    const { itemId } = req.params;
+    const result = await marketService.deleteMarketItem(Number(itemId));
+    return res.status(200).json({
+      message: '판매 카드 삭제 성공',
+      data: result,
+    });
+  }),
 };
