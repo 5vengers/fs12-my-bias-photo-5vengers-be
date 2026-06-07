03017bbd511915b5b34ec4e92affebd9de9c3e5b
Jeongmin <milk001123@naver.com>
2026-06-07 14:44:38 +0900

    fix: Zod v4 issues 필드로 수정

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,10 +1,10 @@
-import { ValidationError } from '../errors/AppError.js';
+import { ValidationError } from '../errors/appError.js';
 
 export const validate = (schema) => (req, res, next) => {
   const result = schema.safeParse(req.body);
 
   if (!result.success) {
-    const message = result.error.errors[0].message;
+    const message = result.error.issues[0].message;
     return next(new ValidationError(message));
   }
 
