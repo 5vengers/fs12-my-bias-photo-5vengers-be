58e2b832af5bde50fe539175c244a10c9872214b
Obebe <shkim000905@naver.com>
2026-06-08 18:29:17 +0900

    feat:pointController 구현

---
+++ src/controllers/pointController.js
@@ -0,0 +1,14 @@
+import pointService from '../services/pointService.js';
+
+const getMyPoint = async (req, res, next) => {
+  try {
+    const userId = await pointService.getMyPoint(req.user.id);
+
+    res.json({
+      success: true,
+      data: point,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
