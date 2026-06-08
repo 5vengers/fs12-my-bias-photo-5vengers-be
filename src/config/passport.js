11ee291ac6e6d9f8ff0c5c17196b1d594cae7e42
Jeongmin <milk001123@naver.com>
2026-06-08 16:50:33 +0900

    fix: 이메일 누락 에러를 OAuthError로 변경

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,7 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import authService from '../services/authService.js';
+import { OAuthError } from '../errors/appError.js';
 
 /**
  * Google OAuth 2.0 Strategy 등록
@@ -22,13 +23,16 @@ passport.use(
 
         if (!email) {
           return done(
-            new Error('Google 계정에서 이메일 정보를 가져올 수 없습니다.'),
+            new OAuthError('Google 계정에서 이메일 정보를 가져올 수 없습니다.'),
           );
         }
 
+        // displayName이 없는 경우 이메일 앞부분을 닉네임 기본값으로 사용
+        const nickname = profile.displayName ?? email.split('@')[0];
+
         const { user, isNewUser } = await authService.findOrCreateGoogleUser({
           email,
-          nickname: profile.displayName,
+          nickname,
           providerId: profile.id,
         });
 
