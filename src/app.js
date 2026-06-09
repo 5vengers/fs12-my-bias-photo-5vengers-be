ddae82f4d9119d10eaf079d61eeebfd8a42cbd56
Obebe <shkim000905@naver.com>
2026-06-10 08:40:20 +0900

    chore: app.js 내부 경로 수정

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth', authRouter);
 app.use('/api/points', pointRouter);
-app.use('api', marketRouter);
+app.use('/api', marketRouter);
 
 // 라우터 등록 후, 404 Not Found 처리 미들웨어와 에러 처리 미들웨어 등록
 app.use(notFoundHandler);
