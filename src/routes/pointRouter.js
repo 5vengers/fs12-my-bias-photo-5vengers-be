6ddb568354a2c4a4861b2a35daf5649d1959a9e4
Obebe <shkim000905@naver.com>
2026-06-10 10:17:55 +0900

    feat:pointRouter에 asyncHandler추가

--- src/routes/pointRouter.js
+++ src/routes/pointRouter.js
@@ -1,18 +1,19 @@
 import { Router } from 'express';
 import pointController from '../controllers/pointController.js';
 import { authenticate } from '../middlewares/authenticate.js';
+import { asyncHandler } from '../middlewares/asyncHandler.js';
 import { validate } from '../middlewares/validate.js';
 import { openPointBoxSchema } from '../schemas/pointBoxSchema.js';
 
 const router = Router();
 
-router.get('/me', authenticate, pointController.getMyPoint);
+router.get('/me', authenticate, asyncHandler(pointController.getMyPoint));
 
 router.post(
   '/box',
   authenticate,
   validate(openPointBoxSchema),
-  pointController.openPointBox,
+  asyncHandler(pointController.openPointBox),
 );
 
 export default router;
