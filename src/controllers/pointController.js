cf0389f6c0b8a5e22e218b72ff5ffc1304daefc2
Obebe <shkim000905@naver.com>
2026-06-10 10:22:03 +0900

    Controller try-catch문 제거

--- src/controllers/pointController.js
+++ src/controllers/pointController.js
@@ -1,32 +1,23 @@
 import pointService from '../services/pointService.js';
 
-const getMyPoint = async (req, res, next) => {
-  try {
-    const { point } = await pointService.getMyPoint(req.user.userId);
+const getMyPoint = async (req, res) => {
+  const { point } = await pointService.getMyPoint(req.user.userId);
 
-    res.json({
-      success: true,
-      data: point,
-    });
-  } catch (err) {
-    next(err);
-  }
+  return res.json({
+    success: true,
+    data: point,
+  });
 };
+const openPointBox = async (req, res) => {
+  const result = await pointService.openPointBox(
+    req.user.userId,
+    req.body.boxNumber,
+  );
 
-const openPointBox = async (req, res, next) => {
-  try {
-    const result = await pointService.openPointBox(
-      req.user.userId,
-      req.body.boxNumber,
-    );
-
-    res.json({
-      success: true,
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
+  return res.json({
+    success: true,
+    data: result,
+  });
 };
 
 export default { getMyPoint, openPointBox };
