d48eab6d9088f27632c3ed7a72b7c63676d85119
9g-g9 <nayun12375@gmail.com>
2026-06-16 22:07:18 +0900

    feat: 마이갤러리 api 개수 확인 수정 작업 및 카드 개수 조회 추가

--- src/repositories/myGalleryRepository.js
+++ src/repositories/myGalleryRepository.js
@@ -1,11 +1,20 @@
 import prisma from '../config/prisma.js';
-import { Genre, CardGrade } from '@prisma/client';
+import { Genre, CardGrade, MarketStatus } from '@prisma/client';
 
 import { PhotoCardLimitError } from '../errors/appError.js';
 import { ERROR_CODES } from '../constants/errorCodes.js';
 
+// sold quantity 계산
+const countSold = (items) => {
+  const count = items
+    .filter((item) => item.status === MarketStatus.SELLING)
+    .reduce((acc, item) => acc + item.quantity, 0);
+
+  return count;
+};
+
 // 내 소유 카드 조회
-const findAllMyCards = async (ownerId, keyword, genre, grade) => {
+const findAllMyCards = async (ownerId, keyword, genre, grade, skip, limit) => {
   // 필터링 : genre, grade, keyword
   // keyword 처리 후 genre, grade 처리
   // genre, grade enum 처리
@@ -15,36 +24,141 @@ const findAllMyCards = async (ownerId, keyword, genre, grade) => {
     : undefined;
 
   // 기본 카드 조회
-  const cards = await prisma.myCard.findMany({
+  const [cards, total] = await Promise.all([
+    prisma.myCard.findMany({
+      where: {
+        ownerId,
+        photoCard: {
+          ...(keyword && {
+            OR: [
+              { name: { contains: keyword } },
+              { description: { contains: keyword } },
+            ],
+          }),
+          ...(genre && { genre: genreValue }),
+          ...(grade && { grade: gradeValue }),
+        },
+      },
+      select: {
+        quantity: true,
+        photoCard: {
+          select: {
+            name: true,
+            description: true,
+            genre: true,
+            grade: true,
+            price: true,
+            imageUrl: true,
+          },
+        },
+        marketItems: {
+          select: {
+            quantity: true,
+          },
+          where: {
+            status: MarketStatus.SELLING,
+          },
+        },
+      },
+      skip,
+      take: limit,
+    }),
+    await prisma.myCard.findMany({
+      where: {
+        ownerId,
+        photoCard: {
+          ...(keyword && {
+            OR: [
+              { name: { contains: keyword } },
+              { description: { contains: keyword } },
+            ],
+          }),
+          ...(genre && { genre: genreValue }),
+          ...(grade && { grade: gradeValue }),
+        },
+      },
+      select: {
+        quantity: true,
+        photoCard: {
+          select: {
+            grade: true,
+          },
+        },
+        marketItems: {
+          select: {
+            quantity: true,
+          },
+          where: {
+            status: MarketStatus.SELLING,
+          },
+        },
+      },
+    }),
+  ]);
+
+  const totalPages = Math.ceil(total.length / limit);
+
+  const data = cards.map(({ quantity, photoCard, marketItems }) => {
+    const soldQuantity = countSold(marketItems);
+    return {
+      quantity: quantity - soldQuantity,
+      ...photoCard,
+    };
+  });
+
+  return {
+    cards: data,
+    totalPages,
+  };
+
+  return result;
+};
+
+// totalCount 및 gradeCount 조회
+const findAllCardCount = async (ownerId) => {
+  const allCards = await prisma.myCard.findMany({
     where: {
       ownerId,
-      photoCard: {
-        ...(keyword && {
-          OR: [
-            { name: { contains: keyword } },
-            { description: { contains: keyword } },
-          ],
-        }),
-        ...(genre && { genre: genreValue }),
-        ...(grade && { grade: gradeValue }),
-      },
     },
     select: {
       quantity: true,
       photoCard: {
         select: {
-          name: true,
-          description: true,
-          genre: true,
           grade: true,
-          price: true,
-          imageUrl: true,
+        },
+      },
+      marketItems: {
+        select: {
+          quantity: true,
+        },
+        where: {
+          status: MarketStatus.SELLING,
         },
       },
     },
   });
 
-  return cards;
+  const totalCount = allCards.reduce((acc, { quantity, marketItems }) => {
+    const soldQuantity = countSold(marketItems);
+    acc += quantity - soldQuantity;
+    return acc;
+  }, 0);
+
+  const gradeCount = allCards.reduce(
+    (acc, { quantity, photoCard, marketItems }) => {
+      const soldQuantity = countSold(marketItems);
+      // acc 를 등급 객체로 만들어 관리하는데 해당 값이 없으면 0 처리
+      acc[photoCard.grade] =
+        (acc[photoCard.grade] || 0) + (quantity - soldQuantity);
+      return acc;
+    },
+    {},
+  );
+
+  return {
+    totalCount,
+    gradeCount,
+  };
 };
 
 // 포토 카드 생성
@@ -128,6 +242,7 @@ const findLimits = async (userId, year, month) => {
 
 export default {
   findAllMyCards,
+  findAllCardCount,
   createCard,
   findLimits,
 };
