2f5f4d5ba3e4f2c0fc963e9c3d4961a266b46227
sebikawa <tpqlsdl150@naver.com>
2026-06-17 08:58:25 +0900

    [Feat]/마켓플레이스 api 로직추가 (#30)
    * feat: 마켓플레이스 crud api 추가
    
    * feat: route/marketRouter.js 추가
    
    * feat: 마켓 controller 에러처리 추가
    
    * feat: 마켓 서비스 예외처리 추가
    
    * 커밋 병합
    
    * feat: asyncHandler 추가 및 라우터 연결
    
    * feat: createMarketItemSchema 생성 후 validate연결
    
    * feat: 마켓 인증 처리
    
    * feat: soft deleted  적용
    
    * chore: 스키마 DELETED로 상태명 수정
    
    * chore: migration
    
    * refactor: 성공 응답 형식 맞춤
    
    * feat: 삭제 상태시 삭제 검증 추가
    
    * feat: orderBy 추가 및 page params추가
    
    * feat: 판매중 상품만 조회 가능 로직 추가
    
    * chore: selling 에서 deleted로 수정
    
    * feat: dto 데이터 추가
    
    * test: test코드 제거
    
    * chore: 오타수정

--- src/routes/marketRouter.js
+++ src/routes/marketRouter.js
@@ -8,25 +8,25 @@ import { authenticate } from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
-router.get('/market/items', asyncHandler(marketController.getMarketItems));
+router.get('/items', asyncHandler(marketController.getMarketItems));
 router.get(
-  '/market/items/:itemId',
+  '/items/:itemId',
   authenticate,
   asyncHandler(marketController.getMarketItemDetail),
 );
 router.post(
-  '/market/items',
+  '/items',
   authenticate,
   validate(createMarketItemSchema),
   asyncHandler(marketController.createMarketItem),
 );
 router.patch(
-  '/market/items/:itemId',
+  '/items/:itemId',
   authenticate,
   asyncHandler(marketController.updateMarketItem),
 );
 router.delete(
-  '/market/items/:itemId',
+  '/items/:itemId',
   authenticate,
   asyncHandler(marketController.deleteMarketItem),
 );
